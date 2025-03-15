0054ef1c8880d0f56a1f8b1243d57d996f0c8c77
Chirag <chiragbambhaniya12@gmail.com>
2025-03-15 10:52:10 +0530

    for

---
+++ constant.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//preprocessor directives
+#define PI 3.14
+main(){
+//	const float pi=3.14;
+	int a=10;
+	a=45;
+	printf("\n Pi is %f",PI);
+	printf("\n A is %d",a);
+}
---
+++ for2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main(){
+	int i,j;
+	for(i=1;i<=5;i++){
+		for(j=1;j<=i;j++){
+//			if(j<=i)
+				printf("*");
+		}
+		printf("\n");
+		
+	}
+}
